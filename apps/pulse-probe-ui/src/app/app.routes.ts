b77eeb5743f4f9f2bed2f647a71b6ecd5c42a066
Ajith C Ravi <aravi@presidio.com>
2025-04-16 07:47:19 +0530

    Moved components to page folder

--- apps/pulse-probe-ui/src/app/app.routes.ts
+++ apps/pulse-probe-ui/src/app/app.routes.ts
@@ -4,12 +4,12 @@ export const appRoutes: Route[] = [
   {
     path: 'login',
     loadComponent: () =>
-      import('./login/login.component').then((m) => m.LoginComponent),
+      import('./pages/auth/login/login.component').then((m) => m.LoginComponent),
   },
   {
     path: 'register',
     loadComponent: () =>
-      import('./register/register.component').then((m) => m.RegisterComponent),
+      import('./pages/auth/register/register.component').then((m) => m.RegisterComponent),
   },
   {
     path: '',
