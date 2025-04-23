e2d8dd60e7888f24b512df545095315232583986
Ajith C Ravi <aravi@presidio.com>
2025-04-24 00:14:28 +0530

    Added health route

--- apps/pulse-probe-ui-api/src/app/app.service.ts
+++ apps/pulse-probe-ui-api/src/app/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Hello API' };
+  getHealth(): { message: string } {
+    return { message: 'Healthy' };
   }
 }
